1b90cf8bb54a5fec50754f017748a98430716303
venkyy04 <165155257+venkyy04@users.noreply.github.com>
2024-03-27 16:30:07 +0530

    Add files via upload
    hi

---
+++ arrayy_2d.java
@@ -0,0 +1,13 @@
+
+public class arrayy_2d {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public void array2D() {
+			int[][] arr2D = {{1, 2, 3},{4, 5, 6}};
+			System.out.println((arr2D));
+		}
+
+	}
+
+}
